0d66c9cc28e5daa9c9ba28f4538fdcad4ab89459
Hero2323 <58619697+Hero2323@users.noreply.github.com>
2021-06-09 15:31:19 +0200

    Tested the deallocate function

--- Code/scheduler.c
+++ Code/scheduler.c
@@ -78,21 +78,88 @@ int main(int argc, char *argv[])
 {
     printf("Scheduler spawned!\n");
 
-    initClk();
+    //initClk();
     // if the scheduler receives this signal, it means that the PG is sending it a new process
     signal(SIGUSR1, new_process_handler);
+
+    theAlgorithm = atoi(argv[1]);
+    if (theAlgorithm == RR)
+    {
+        QUANTUM = atoi(argv[3]);
+        memAlgo = atoi(argv[4]);
+    }
+    else
+    {
+        QUANTUM = 0;
+        memAlgo = atoi(argv[3]);
+    }
+
     //******************************MEMORY INIT*************************//
+
     void (*MemAlgoToRun)(void);
     switch (memAlgo)
     {
     case FF:
         MemoryList = createList(&dummy_compare);
+        
         memory_fragment *initMem;
         initMem = (memory_fragment *)malloc(sizeof(memory_fragment));
         initMem->theState = GAP;
         initMem->start_position = 0;
         initMem->length = 1024;
-        _insert(MemoryList, MemoryList->rear, (void *)initMem);
+        //_insert(MemoryList, MemoryList->rear, (void *)initMem);
+        memory_fragment * m1 = (memory_fragment *)malloc(sizeof(memory_fragment));
+        memory_fragment * m2 = (memory_fragment *)malloc(sizeof(memory_fragment));
+        memory_fragment * m3 = (memory_fragment *)malloc(sizeof(memory_fragment));
+        memory_fragment * m4 = (memory_fragment *)malloc(sizeof(memory_fragment));
+        m1->start_position = 0;
+        m1->theState = PROCESS;
+        m1->id = 1;
+        m1->length = 100;
+
+        m2->start_position = 100;
+        m2->theState = GAP;
+        m2->id = 2;
+        m2->length = 100;
+
+        m3->start_position = 200;
+        m3->theState = PROCESS;
+        m3->id = 3;
+        m3->length = 100;
+
+        m4->start_position = 300;
+        m4->theState = PROCESS;
+        m4->id = 4;
+        m4->length = 100;
+        
+
+        
+        _insert(MemoryList, MemoryList->rear, (void *) m1);
+        _insert(MemoryList, MemoryList->rear, (void *) m2);
+        _insert(MemoryList, MemoryList->rear, (void *) m3);
+        _insert(MemoryList, MemoryList->rear, (void *) m4);
+
+        NODE * ptr = MemoryList->head;
+        while (ptr != NULL)
+        {
+            printf("Process with ID %d starts at pos %d and ends at pos %d and state %d\n",
+            ((memory_fragment *) ptr->dataPtr )->id, ((memory_fragment *) ptr->dataPtr )->start_position,
+            ((memory_fragment *) ptr->dataPtr )->start_position + ((memory_fragment *) ptr->dataPtr )->length - 1,
+            ((memory_fragment *) ptr->dataPtr )->theState);
+            ptr = ptr->link;
+        }
+
+        deallocateMemory(3);
+        ptr = MemoryList->head;
+        while (ptr != NULL)
+        {
+            printf("Process with ID %d starts at pos %d and ends at pos %d and state %d\n",
+            ((memory_fragment *) ptr->dataPtr )->id, ((memory_fragment *) ptr->dataPtr )->start_position,
+            ((memory_fragment *) ptr->dataPtr )->start_position + ((memory_fragment *) ptr->dataPtr )->length - 1,
+            ((memory_fragment *) ptr->dataPtr )->theState);
+            ptr = ptr->link;
+        }
+
         MemAlgoToRun = &First_Fit_memAlgo;
         //todo: function_pointer= FF algo
         break;
@@ -181,7 +248,6 @@ int main(int argc, char *argv[])
         printf("Error opening log file\n");
 
     printf("The number of Processes to schedule is %d\n", numOfProcs);
-
     // TODO: depending on the value of sigAlgo, make the required Datastructures & use them
     // the Algorithm should be equal to the wanted Algorithm (from 1 to 5 as in the document)
     //1. First Come First Serve (FCFS)
@@ -190,17 +256,7 @@ int main(int argc, char *argv[])
     //4. Shortest Remaining Time Next (SRTN)
     //5. Round Robin (RR)
     // we should determine the algo from the args in process generator
-    theAlgorithm = atoi(argv[1]);
-    if (theAlgorithm == RR)
-    {
-        QUANTUM = atoi(argv[3]);
-        memAlgo = atoi(argv[4]);
-    }
-    else
-    {
-        QUANTUM = 0;
-        memAlgo = atoi(argv[3]);
-    }
+    
 
     //printf("Quantum= %d\n", QUANTUM);
 
@@ -889,7 +945,7 @@ void deallocateMemory(int process_id) // only called for ff,nf,bf
     NODE* prev = ptr; // ptr to prev node;
     NODE* temp;
     
-    while (ptr != NULL || found)
+    while (ptr != NULL && !found)
     {
         if ( ( (memory_fragment *) ptr->dataPtr )->id == process_id )
         {
@@ -935,6 +991,7 @@ void deallocateMemory(int process_id) // only called for ff,nf,bf
                 if (( (memory_fragment *) prev->dataPtr )->theState == GAP)
                 {
                     ( (memory_fragment *) prev->dataPtr )->length += ( (memory_fragment *) ptr->dataPtr )->length;
+                    ( (memory_fragment *) prev->dataPtr )->id = -1;
                     prev->link = ptr->link;
                     free(ptr->dataPtr);
                     free(ptr);
@@ -957,6 +1014,7 @@ void deallocateMemory(int process_id) // only called for ff,nf,bf
                 {
                     ( (memory_fragment *) prev->dataPtr )->length += ( (memory_fragment *) ptr->dataPtr )->length +
                     ( (memory_fragment *) temp->dataPtr )->length;
+                    ( (memory_fragment *) prev->dataPtr )->id = -1;
                     prev->link = temp->link;
                     if (MemoryList->rear == temp)
                         MemoryList->rear = prev;
@@ -970,6 +1028,7 @@ void deallocateMemory(int process_id) // only called for ff,nf,bf
                 ( (memory_fragment *) temp->dataPtr )->theState != GAP)
                 {
                     ( (memory_fragment *) prev->dataPtr )->length += ( (memory_fragment *) ptr->dataPtr )->length;
+                    ( (memory_fragment *) prev->dataPtr )->id = -1;
                     prev->link = ptr->link;
                     free(ptr->dataPtr);
                     free(ptr);
@@ -1009,4 +1068,5 @@ void deallocateMemory(int process_id) // only called for ff,nf,bf
 }
 void deallocateMemory_BSA(int process_id) //ony for BSA
 {
+
 }
